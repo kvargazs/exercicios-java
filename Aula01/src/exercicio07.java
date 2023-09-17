cfe20cb17a202d38d5a93674f8194329bb2450f8
Karoline <133781521+kvargazs@users.noreply.github.com>
2023-09-17 18:06:28 -0300

    Add files via upload

--- Aula01/src/exercicio07.java
+++ Aula01/src/exercicio07.java
@@ -1,4 +1,4 @@
-// 07. Criar para uma empresa de consórcio. No teste inicial, você deve perguntar para o usuário a idade dele e o seu salário. A pessoa só pode comprar um automóvel se for de maior e ganhar mais de R$ 2.000,00.
+// Criar para uma empresa de consórcio. No teste inicial, você deve perguntar para o usuário a idade dele e o seu salário. A pessoa só pode comprar um automóvel se for de maior e ganhar mais de R$ 2.000,00.
 
 import java.util.Scanner;
 
