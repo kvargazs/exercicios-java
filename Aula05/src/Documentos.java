710babb532a23a1cc2e70a66e2619aa7eaae5c64
Karoline <karol.v.0570@gmail.com>
2023-10-04 15:10:30 -0300

    update

---
+++ Aula05/src/Documentos.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class Documentos {
+    private String nome;
+    private int numeroPaginas;
+
+    public Documentos(String nome, int numeroPaginas) {
+        this.nome = nome;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+}
