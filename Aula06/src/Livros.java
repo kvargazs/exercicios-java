748a333f28ba99702eb4c1c62a7ea148620902fd
Karoline <karol.v.0570@gmail.com>
2023-10-04 15:51:08 -0300

    update

---
+++ Aula06/src/Livros.java
@@ -0,0 +1,17 @@
+class Livros {
+    private String titulo;
+    private String categoria;
+
+    public Livros(String titulo, String categoria) {
+        this.titulo = titulo;
+        this.categoria = categoria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+}
