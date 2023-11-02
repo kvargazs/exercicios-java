ab0c19ce8a0e98bafaf88075ff59a9849381210d
kvargazs <karol.v.0570@gmail.com>
2023-11-02 13:12:47 -0300

    Aula 07

---
+++ Aula07/src/Conta.java
@@ -0,0 +1,16 @@
+public class Conta {
+    private double saldo;
+
+    public Conta(double saldoInicial) {
+        saldo = saldoInicial;
+    }
+    public void sacar(double valor) throws SaldoInsuficienteException {
+        if (valor > saldo) {
+            throw new SaldoInsuficienteException("Saldo insuficiente para sacar " + valor);
+        }
+        saldo -= valor;
+        System.out.println("Saque de " + valor + " realizado com sucesso. Novo saldo: " + saldo);
+    }
+}
+
+
--- Aula07/src/Exercicio06.java
+++ Aula07/src/Exercicio06.java
@@ -1,2 +1,18 @@
 public class Exercicio06 {
+    public static void main(String[] args) {
+        Conta conta = new Conta(1000.0);
+
+        try {
+            conta.sacar(1200.0);
+        } catch (SaldoInsuficienteException e) {
+            System.err.println("Erro: " + e.getMessage());
+        }
+
+        try {
+            conta.sacar(800.0);
+        } catch (SaldoInsuficienteException e) {
+            System.err.println("Erro: " + e.getMessage());
+        }
+    }
 }
+
