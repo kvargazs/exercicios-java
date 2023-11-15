e2a6ce935c2e017f508681c53e8a0ec55fe04642
kvargazs <karol.v.0570@gmail.com>
2023-11-15 20:04:49 -0300

    Aula 08, exercicio 03

---
+++ Aula08/Exercicio03/src/main/java/Calculadora.java
@@ -0,0 +1,21 @@
+public class Calculadora {
+
+    public double adicao(double a, double b) {
+        return a + b;
+    }
+    public double subtracao(double a, double b) {
+        return a - b;
+    }
+    public double multiplicacao(double a, double b) {
+        return a * b;
+    }
+    public double divisao(double a, double b) {
+        return a / b;
+    }
+    public double raizQuadrada(double a) {
+        return Math.sqrt(a);
+    }
+    public double potencia(double base, double expoente) {
+        return Math.pow(base, expoente);
+    }
+}
