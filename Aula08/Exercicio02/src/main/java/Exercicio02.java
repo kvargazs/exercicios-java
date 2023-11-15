50a169717307a116e052a26e2b5eb6ad92b10d4e
kvargazs <karol.v.0570@gmail.com>
2023-11-15 19:47:32 -0300

    Correções

--- Aula08/Exercicio02/src/main/java/Exercicio02.java
+++ Aula08/Exercicio02/src/main/java/Exercicio02.java
@@ -1,2 +1,33 @@
+import java.util.List;
+
 public class Exercicio02 {
+    public static void main(String[] args) {
+        List<Produto> listaDeProdutos = criarListaDeProdutos();
+
+        //acha o produto mais caro da lista
+        Produto produtoMaisCaro = listaDeProdutos.stream()
+                .max((p1, p2) -> Double.compare(p1.getPreco(), p2.getPreco()))
+                .orElse(null);
+        System.out.println("Produto mais caro: " + produtoMaisCaro);
+
+        //calcula o preço de todos os produtos da lista
+        double precoTotal = listaDeProdutos.stream()
+                .mapToDouble(Produto::getPreco)
+                .sum();
+        System.out.println("Preço total dos produtos: " + precoTotal);
+
+        //filtra os produtos com quantidade maior a 10 e cria uma lista com eles
+        List<Produto> produtosQuantidadeSuperior10 = listaDeProdutos.stream()
+                .filter(p -> p.getQuantidade() > 10)
+                .toList();
+        System.out.println("Produtos com quantidade superior a 10: " + produtosQuantidadeSuperior10);
+    }
+
+    private static List<Produto> criarListaDeProdutos() {
+        return List.of(
+                new Produto("Celular", 2000.0, 7),
+                new Produto("Notebook Dell G15", 5000, 5),
+                new Produto("Coca-Cola 2L", 10, 20)
+        );
+    }
 }
---
+++ Aula08/Exercicio02/src/main/java/Produto.java
@@ -0,0 +1,23 @@
+public class Produto {
+    private final String nome;
+    private final double preco;
+    private final int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+}
